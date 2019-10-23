db86dfb4a1c4672ca9b0930b1c9874a9d79da6c1
nuris akbar <nurisakbar@globalvillage.co.id>
2019-10-23 15:24:05 +0100

    perbaikan redirectTo menjadi admin/penduduk

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/penduduk';
+    protected $redirectTo = '/admin/penduduk';
 
     /**
      * Create a new controller instance.
