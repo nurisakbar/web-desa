8bbc3389b96da863c73b4d519d2c4fcb8a31d377
nuris akbar <nurisakbar@globalvillage.co.id>
2019-11-09 23:34:15 +0700

    Update Penduduk.php

--- app/Models/Penduduk.php
+++ app/Models/Penduduk.php
@@ -8,5 +8,10 @@ class Penduduk extends Model
 {
     protected $table='penduduk';
 
-    protected $fillable= ['no_kk','nama','nama_ayah','nama_ibu','jenis_kelamin','tempat_lahir','tanggal_lahir','agama_id','pendidikan_id','jenis_pekerjaan_id','status_kawin_id','no_password','no_kitab','nik',];
+    // public function getTanggalLahirAttribute($value)
+    // {
+    //     return date_format(date_create($value),"m-d-Y");
+    // }
+
+    protected $fillable= ['no_kk','nama','nama_ayah','nama_ibu','jenis_kelamin','tempat_lahir','tanggal_lahir','agama_id','pendidikan_id','jenis_pekerjaan_id','status_kawin_id','no_passport','no_kitap','nik','status_hubungan'];
 }
